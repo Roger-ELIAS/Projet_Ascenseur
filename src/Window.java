3ffde1a8b45f5eda64616c048c5c183b3b491a31
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2019-10-02 21:57:21 +0200

    update

---
+++ src/Window.java
@@ -0,0 +1,40 @@
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class Window {
+    @FXML
+    private Label messagesText ;
+
+    @FXML
+    private void fullStop(Event event) {
+        Button btn = (Button) event.getSource();
+//        String id = btn.getId();
+        messagesText.setText("EMERGENCY STOP");
+    }
+
+    @FXML
+    private void goUp(Event event) {
+        Button btn = (Button) event.getSource();
+        String id = btn.getId();
+        String result = id.substring(7);
+        messagesText.setText("PERSON AT FLOOR " + result + " WANT TO GO UP");
+    }
+
+    @FXML
+    private void goDown(Event event) {
+        Button btn = (Button) event.getSource();
+        String id = btn.getId();
+        String result = id.substring(9);
+        messagesText.setText("PERSON AT FLOOR " + result + " WANT TO GO DOWN");
+    }
+
+    @FXML
+    private void goTo(Event event) {
+        Button btn = (Button) event.getSource();
+        String id = btn.getId();
+        String result = id.substring(3);
+        messagesText.setText("MAKE LIFT GO TO FLOOR " + result);
+    }
+}
