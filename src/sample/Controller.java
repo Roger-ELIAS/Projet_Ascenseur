93d40440fd9760ce81a1ee5556ca9138ebb4d811
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2019-10-02 21:43:06 +0200

    Boutons

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,8 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class Controller {
+
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,16 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
+
+import javax.swing.text.View;
+import java.awt.event.ActionEvent;
+import java.beans.EventHandler;
 
 public class Main extends Application {
 
@@ -12,9 +18,11 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 960, 540));
         primaryStage.show();
     }
+
+
 
 
     public static void main(String[] args) {
