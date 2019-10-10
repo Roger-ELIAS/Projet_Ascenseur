884d80b70c489e2e39bd05bc2270512cca808ec1
Edouard Pigot <edouard.pigot@etu.univ-amu.fr>
2019-10-10 17:05:41 +0200

    Generation auto étages

--- src/Window.java
+++ src/Window.java
@@ -1,21 +1,26 @@
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 
 public class Window {
     @FXML
     private Label messagesText ;
 
     @FXML
-    private void fullStop(Event event) {
+    public void fullStop(Event event) {
         Button btn = (Button) event.getSource();
 //        String id = btn.getId();
         messagesText.setText("EMERGENCY STOP");
     }
 
     @FXML
-    private void goUp(Event event) {
+    public void goUp(Event event) {
         Button btn = (Button) event.getSource();
         String id = btn.getId();
         String result = id.substring(7);
@@ -23,18 +28,24 @@ public class Window {
     }
 
     @FXML
-    private void goDown(Event event) {
+    public void goDown(Event event) {
         Button btn = (Button) event.getSource();
         String id = btn.getId();
         String result = id.substring(9);
+        btn.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
         messagesText.setText("PERSON AT FLOOR " + result + " WANT TO GO DOWN");
     }
 
     @FXML
-    private void goTo(Event event) {
+    public void goTo(Event event) {
         Button btn = (Button) event.getSource();
         String id = btn.getId();
         String result = id.substring(3);
         messagesText.setText("MAKE LIFT GO TO FLOOR " + result);
+    }
+
+    @FXML
+    public void changeButtonColor(int index, boolean stat){
+
     }
 }
